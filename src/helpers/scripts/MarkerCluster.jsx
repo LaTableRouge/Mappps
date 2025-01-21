550500ed2f385c37c16ae0b0ab0fe846f52f50ae
MLNOP <victordianamlnop@gmail.com>
2025-01-21 09:18:00 +0100

    feat(markercluster): wip localize package

--- src/helpers/scripts/MarkerCluster.jsx
+++ src/helpers/scripts/MarkerCluster.jsx
@@ -1,20 +1,31 @@
-import 'leaflet.markercluster'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+
+import './leaflet.markercluster'
 
 import { createPathComponent } from '@react-leaflet/core'
 import L from 'leaflet'
 
+import { MarkerClusterGroup } from './leaflet.markercluster'
+
 function createMarkerCluster({ children: _c, ...props }, context) {
   const clusterProps = {}
   const clusterEvents = {}
+
   // Splitting props and events to different objects
-  Object.entries(props).forEach(([propName, prop]) => (propName.startsWith('on') ? (clusterEvents[propName] = prop) : (clusterProps[propName] = prop)))
-  const instance = new L.MarkerClusterGroup(clusterProps)
+  Object.entries(props).forEach(([propName, prop]) =>
+    (propName.startsWith('on') ? (clusterEvents[propName] = prop) : (clusterProps[propName] = prop))
+  )
+
+  // Create a new instance for each component
+  const instance = new MarkerClusterGroup(clusterProps)
 
   // Initializing event listeners
   Object.entries(clusterEvents).forEach(([eventAsProp, callback]) => {
     const clusterEvent = `cluster${eventAsProp.substring(2).toLowerCase()}`
     instance.on(clusterEvent, callback)
   })
+
   return {
     instance,
     context: {
